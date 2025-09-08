20839ec734268a1e573643f68782edc6380c21e3
adrielecaldas-alt <adriele.caldas@sempreceub.com>
2025-09-08 12:31:54 -0300

    Add files via upload

---
+++ aula 1/nome.py
@@ -0,0 +1,19 @@
+
+# 1)
+x = input("Digite seu nome: ")
+print ("Ola " + x )
+
+# 2)
+n1 = float(input("Digite o numero "))
+n2 = float(input(" Digite o numero "))
+print( n1 + n2)
+
+# 3)
+
+b = int(input(" Base do retangulo:"))
+h = int(input(" Altura do retangulo:"))
+print(' A area do retangulo é' , b * h)
+
+# 4)
+n = int(input("Digite o numero:"))
+print ('O sucessor de',  n + 1 , 'O antecessor de', n - 1)
