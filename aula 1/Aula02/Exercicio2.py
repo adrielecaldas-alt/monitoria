2a481d4d726215e49c98aab4c4f6d1ec491cd933
adrielecaldas-alt <adriele.caldas@sempreceub.com>
2025-09-10 23:57:16 -0300

    Delete aula 1/Aula02 directory

--- aula 1/Aula02/Exercicio2.py
+++
@@ -1,6 +0,0 @@
-# 2)
-n = int(input('Digie o numero:'))
-if n % 2 == 0:
-    print('O numero é par:', n)
-else:
-    print('O numero é impar:', n) 
--- aula 1/Aula02/Exercicio3.py
+++
@@ -1,8 +0,0 @@
-# 3)
-i = int(input('Digite a idade:'))
-if i <= 12:
-    print('Criança:', i)
-elif 18 >= i <= 59:
-    print('Adulto', i)
-else:
-    print('Idoso', i)    
